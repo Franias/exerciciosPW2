58081e0ebc949eea6107e321ad48532c0da8e931
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2022-12-16 16:12:43 -0300

    jwt com as keys

---
+++ user/src/main/java/Service.java
@@ -0,0 +1,22 @@
+import javax.ws.rs.Path;
+
+@Path("/jwt/{user}/{password}")
+public class Service {
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+
+    public String generate(@PathParam("user") String user,)
+                            @PathParam("password") String password {
+                                //aqui é o ponto que vc verifica se
+                                //o usuario existe
+        System.out.println(user);
+        System.out.println(password);
+        return Jwt.issuer("http://localhost:8080")
+                .upn("chavePodeSerQualquerInfoComoEmail")
+                .groups(new HashSet<>(Arrays.asList("User", "Admin"))) // nao tem role de admin, ele vai gerar um erro de 403
+                .claim(Claims.full_name, "Francielli Pinheiro Dias")
+                .sign();
+    }
+    
+}
